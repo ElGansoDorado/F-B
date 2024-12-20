d089dd22c54ed7d3c22881ba0f5236bbcbcdc47e
ElCarambola <yakovchikdenis@gmail.com>
2024-12-20 22:04:02 +0300

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import "./Header.scss"
+
+function Header()
+{
+    return(
+    <header>
+        <div className="logo"></div>
+
+        <div className="menu"></div>
+
+        <div className="icon"></div>
+    </header>
+    )
+}
+
+export default Header
