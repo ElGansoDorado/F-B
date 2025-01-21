3b5fc82786664903c1bbd79e2c5bf33232b880ec
ElCarambola <yakovchikdenis@gmail.com>
2025-01-21 14:37:36 +0300

    fix: Menu

--- src/components/header/menu/Menu.jsx
+++ src/components/header/menu/Menu.jsx
@@ -5,17 +5,25 @@ import { useState } from "react"
 function Menu() {
     const [isActive, setActive] = useState(false)
 
+    function SwitchScroll(isIncluded)
+    {
+        setActive(isIncluded)
+
+        var element = document.getElementById("body");
+        element.classList.toggle("lock");
+    }
+
     return (
         <>
-        <div onClick={() => {setActive(!isActive)}} className={isActive ? "burger active" : "burger"}>
+        <div onClick={() => {SwitchScroll(!isActive)}} className={isActive ? "burger active" : "burger"}>
             <span></span>
         </div>
         <nav>
             <ul className={isActive ? "menu active" : "menu"}>
-                <MenuButton onClick={() => {setActive(false)}} sectionRef={'#catalog'}>Каталог</MenuButton>
-                <MenuButton onClick={() => {setActive(false)}} sectionRef={'#gallery'}>Галерея</MenuButton>
-                <MenuButton onClick={() => {setActive(false)}} sectionRef={'#about'}>О лаборатории</MenuButton>
-                <MenuButton onClick={() => {setActive(false)}} sectionRef={'#contacts'}>Контакты</MenuButton>
+                <MenuButton onClick={() => {SwitchScroll(false)}} sectionRef={'#catalog'}>Каталог</MenuButton>
+                <MenuButton onClick={() => {SwitchScroll(false)}} sectionRef={'#gallery'}>Галерея</MenuButton>
+                <MenuButton onClick={() => {SwitchScroll(false)}} sectionRef={'#about'}>О лаборатории</MenuButton>
+                <MenuButton onClick={() => {SwitchScroll(false)}} sectionRef={'#contacts'}>Контакты</MenuButton>
             </ul>
         </nav>
         </>
